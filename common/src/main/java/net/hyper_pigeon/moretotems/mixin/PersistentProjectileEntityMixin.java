1c92779dfd45920469e10183e10d710b4189168d
HyperPigeon <58276133+HyperPigeon@users.noreply.github.com>
2024-04-21 21:39:43 -0400

    Ported to 1.20.1

--- common/src/main/java/net/hyper_pigeon/moretotems/mixin/PersistentProjectileEntityMixin.java
+++ common/src/main/java/net/hyper_pigeon/moretotems/mixin/PersistentProjectileEntityMixin.java
@@ -1,12 +1,10 @@
 package net.hyper_pigeon.moretotems.mixin;
 
-import net.hyper_pigeon.moretotems.MoreTotemsMod;
 import net.hyper_pigeon.moretotems.register.StatusEffectRegistry;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.projectile.AbstractArrow;
-import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -26,8 +24,8 @@ public abstract class PersistentProjectileEntityMixin extends Entity {
     }
 
 
-    @Inject(method = "<init>(Lnet/minecraft/world/entity/EntityType;Lnet/minecraft/world/entity/LivingEntity;Lnet/minecraft/world/level/Level;Lnet/minecraft/world/item/ItemStack;)V", at = @At("RETURN"))
-    private void onInit(EntityType $$0, LivingEntity owner, Level $$2, ItemStack $$3, CallbackInfo ci) {
+    @Inject(method = "<init>(Lnet/minecraft/world/entity/EntityType;Lnet/minecraft/world/entity/LivingEntity;Lnet/minecraft/world/level/Level;)V", at = @At("RETURN"))
+    private void onInit(EntityType $$0, LivingEntity owner, Level $$2, CallbackInfo ci) {
         if (owner.hasEffect(StatusEffectRegistry.SNIPER.get())) {
             this.baseDamage = baseDamage * 2;
         }
